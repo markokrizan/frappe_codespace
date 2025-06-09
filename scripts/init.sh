0a8642b0b7e6dd141829b7ced3eb3ec4cb38a089
Marko Krizan <markokrizan64@gmail.com>
2025-06-09 16:24:50 +0200

    Setup env for frappe/crm development

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,16 +1,16 @@
-#!bin/bash
+#!/bin/bash
 
 set -e
 
-if [[ -f "/workspaces/frappe_codespace/frappe-bench/apps/frappe" ]]
+if [[ -f "/workspace/frappe-bench/apps/frappe" ]]
 then
     echo "Bench already exists, skipping init"
     exit 0
 fi
 
-rm -rf /workspaces/frappe_codespace/.git
 
 source /home/frappe/.nvm/nvm.sh
+nvm install 18
 nvm alias default 18
 nvm use 18
 
@@ -20,15 +20,16 @@ cd /workspace
 bench init \
 --ignore-exist \
 --skip-redis-config-generation \
+--frappe-branch version-14 \
 frappe-bench
 
 cd frappe-bench
 
 # Use containers instead of localhost
 bench set-mariadb-host mariadb
-bench set-redis-cache-host redis-cache:6379
-bench set-redis-queue-host redis-queue:6379
-bench set-redis-socketio-host redis-socketio:6379
+bench set-redis-cache-host redis://redis-cache:6379
+bench set-redis-queue-host redis://redis-queue:6379
+bench set-redis-socketio-host redis://redis-socketio:6379
 
 # Remove redis from Procfile
 sed -i '/redis/d' ./Procfile
